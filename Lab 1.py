f7635220b85e5df89657eca1969b246af51d2c9a
ZainnQureshii <ZainnQureshii@users.noreply.github.com>
2018-09-26 21:47:26 -0700

    AI LAB

--- Lab 1.py
+++ Lab 1.py
@@ -1,23 +1,32 @@
-#Task 1 **************
+# #Task 1 **************
+#
+# print ("\nName: Zain Qureshi \nDesignation: Student \nPhone: +92-333-1234567")
+# print ("\nEducation: \n 1. School \n 2. College \n 3. University")
+# print ("\nExperience: \n 1. Internship \n 2. Web Developer \n 3. Technical Lead")
+# print ("\nProjects: \n 1. Project 1 \n 2. Project 2 \n 3. Project 3")
+#
+# #Task 2 **************
+# a = int(input())
+#
+# if a <= 3:
+#     print("Spring")
+#
+# elif a <= 6:
+#     print("Autumn")
+#
+# elif a <= 9:
+#     print("Summer")
+#
+# elif a <= 12:
+#     print("Fall")
+#
+# else: print("Invalid month")
 
-print ("\nName: Zain Qureshi \nDesignation: Student \nPhone: +92-333-1234567")
-print ("\nEducation: \n 1. School \n 2. College \n 3. University")
-print ("\nExperience: \n 1. Internship \n 2. Web Developer \n 3. Technical Lead")
-print ("\nProjects: \n 1. Project 1 \n 2. Project 2 \n 3. Project 3")
+# #Even ODD Program **************
+# a = int(input())
+# if a % 2 == 0:
+#     print("Even")
+#
+# elif a % 2 == 1:
+#     print("Odd")
 
-#Task 2 **************
-a = int(input())
-
-if a <= 3:
-    print("Spring")
-
-elif a <= 6:
-    print("Autumn")
-
-elif a <= 9:
-    print("Summer")
-
-elif a <= 12:
-    print("Fall")
-
-else: print("Invalid month")
