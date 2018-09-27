3ef18c7c6e3cb992eab5eb8bf5c21f864668bcb2
ZainnQureshii <ZainnQureshii@users.noreply.github.com>
2018-09-26 21:39:51 -0700

    AI LAB

---
+++ Lab 1.py
@@ -0,0 +1,23 @@
+#Task 1 **************
+
+print ("\nName: Zain Qureshi \nDesignation: Student \nPhone: +92-333-1234567")
+print ("\nEducation: \n 1. School \n 2. College \n 3. University")
+print ("\nExperience: \n 1. Internship \n 2. Web Developer \n 3. Technical Lead")
+print ("\nProjects: \n 1. Project 1 \n 2. Project 2 \n 3. Project 3")
+
+#Task 2 **************
+a = int(input())
+
+if a <= 3:
+    print("Spring")
+
+elif a <= 6:
+    print("Autumn")
+
+elif a <= 9:
+    print("Summer")
+
+elif a <= 12:
+    print("Fall")
+
+else: print("Invalid month")
